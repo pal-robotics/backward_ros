4c7e9b68969685098e83f190720139c11f235b57
Victor Lopez <victor.lopez@pal-robotics.com>
2018-04-26 15:30:36 +0200

    Resolve relative paths generated by catkin_make

--- cmake/BackwardConfigCatkin.cmake
+++ cmake/BackwardConfigCatkin.cmake
@@ -11,4 +11,20 @@ if (ROSCPP_FOUND)
     add_definitions(-DBACKWARD_HAS_ROSCPP=1)
 endif()
 set(backward_ros_LIBRARIES "") #This is used by catkin, but we don't need it since we force it below
+foreach(lib ${backward_ros_forced_LIBRARIES})
+    if(NOT EXISTS ${lib})
+        message("${lib} doesn't exist, trying to find it")
+        find_file(lib_path 
+            NAMES "lib${lib}.so" 
+            PATHS ${backward_ros_PREFIX})
+            message("XXXXXXXXXXXXXXX ${lib_path} not found")
+        if(NOT ${lib_path})
+            message("${lib} not found")
+        else()
+            message("${lib_path} not found")
+        endif()
+
+    endif()
+endforeach()
 SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-as-needed ${backward_ros_forced_LIBRARIES} -Wl,--as-needed ${CMAKE_EXE_LINKER_FLAGS}")
+
