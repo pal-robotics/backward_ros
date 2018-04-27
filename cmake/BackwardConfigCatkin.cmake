1756224f90ee0a3c26263dda2a32dbea041e2c17
Victor Lopez <victor.lopez@pal-robotics.com>
2018-04-27 08:30:18 +0200

    Fixed version for finding libraries for catkin_make

--- cmake/BackwardConfigCatkin.cmake
+++ cmake/BackwardConfigCatkin.cmake
@@ -1,9 +1,9 @@
 if (LIBDW_FOUND)
     add_definitions(-DBACKWARD_HAS_DW=1)
-    set(backward_ros_forced_LIBRARIES "${backward_ros_LIBRARIES} ${LIBDW_LIBRARIES}")
+    set(backward_ros_forced_LIBRARIES "${backward_ros_LIBRARIES};${LIBDW_LIBRARIES}")
 elseif(LIBBFD_FOUND)
     add_definitions(-DBACKWARD_HAS_BFD=1)
-    set(backward_ros_forced_LIBRARIES "${backward_ros_LIBRARIES} ${LIBBFD_LIBRARIES}")
+    set(backward_ros_forced_LIBRARIES "${backward_ros_LIBRARIES};${LIBBFD_LIBRARIES}")
 else()
     set(backward_ros_forced_LIBRARIES "${backward_ros_LIBRARIES}")
 endif()
@@ -11,20 +11,23 @@ if (ROSCPP_FOUND)
     add_definitions(-DBACKWARD_HAS_ROSCPP=1)
 endif()
 set(backward_ros_LIBRARIES "") #This is used by catkin, but we don't need it since we force it below
+set(backward_ros_full_path_LIBRARIES "") #This is used by catkin, but we don't need it since we force it below
+
+#Hack to find absolute path to libraries
 foreach(lib ${backward_ros_forced_LIBRARIES})
     if(NOT EXISTS ${lib})
-        message("${lib} doesn't exist, trying to find it")
-        find_file(lib_path 
-            NAMES "lib${lib}.so" 
+        message("${lib} doesn't exist, trying to find it in ${backward_ros_PREFIX}")
+        find_library(backward_ros_lib_path 
+            NAMES ${lib} 
             PATHS ${backward_ros_PREFIX})
-            message("XXXXXXXXXXXXXXX ${lib_path} not found")
-        if(NOT ${lib_path})
+        if(NOT ${backward_ros_lib_path})
             message("${lib} not found")
         else()
-            message("${lib_path} not found")
+            message("${backward_ros_lib_path} found")
+            set(backward_ros_full_path_LIBRARIES "${backward_ros_lib_path} ${backward_ros_full_path}")
         endif()
-
+    else()
+        set(backward_ros_full_path_LIBRARIES "${lib} ${backward_ros_full_path}")
     endif()
 endforeach()
-SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-as-needed ${backward_ros_forced_LIBRARIES} -Wl,--as-needed ${CMAKE_EXE_LINKER_FLAGS}")
-
+SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-as-needed ${backward_ros_full_path_LIBRARIES} -Wl,--as-needed ${CMAKE_EXE_LINKER_FLAGS}")
